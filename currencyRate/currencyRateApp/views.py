5a70962c8a194ef69080474bd124359dfaeb684c
Diogo <diogo.campos5@gmail.com>
2024-06-20 13:22:08 +0100

    Save

--- currencyRate/currencyRateApp/views.py
+++ currencyRate/currencyRateApp/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 import requests
-from datetime import date
+from datetime import date, timedelta
 from .models import ExchangeRate
 from django.utils.dateparse import parse_date
 
@@ -21,7 +21,6 @@ def fetch_and_store_historical_rate(query_date):
 def get_usd_to_eur_rate(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
-    single_date = request.GET.get('date')
 
     # if no date is specified -> expose latest currency rate
     if not request.GET:
@@ -31,16 +30,27 @@ def get_usd_to_eur_rate(request):
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
     
-    # if a single date is specified -> expose currency rate of specified date
-    if single_date:
-        single_date = parse_date(single_date)
+    if start_date and end_date:
+            start_date = parse_date(start_date)
+            end_date = parse_date(end_date)
 
-        if not single_date:
-            return JsonResponse({'error': 'Invalid format. Use YYYY-MM-DD.'}, status=400)
-        
-        try:
-            rate = fetch_and_store_historical_rate(single_date)
-            return JsonResponse({'USD_EUR': rate})
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            if not start_date or not end_date:
+                return JsonResponse({'error': 'Invalid format. Use YYYY-MM-DD.'}, status=400)
+                
+            date_range = [start_date + timedelta(days=1) for i in range((end_date - start_date).days + 1)]
+            rates = []
 
+            for single_date in date_range:
+                exchange_rate, created = ExchangeRate.objects.get_or_create(date=single_date)
+                if not created:
+                    try:
+                        rate = fetch_and_store_historical_rate(single_date)
+                    except Exception as e:
+                        return JsonResponse({'error': str(e)}, status=500)
+                rates.append(exchange_rate.rate)
+                        
+            if not rates:
+                return JsonResponse({'error': 'No exchange rate data found for the given date range.'}, status=404)
+            
+            average_rate = sum(rates) / len(rates)
+            return JsonResponse({'average_USD_EUR': average_rate})
