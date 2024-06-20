a76d62673db0995faf2d29a3a16b20c0ac9cdddc
Diogo <diogo.campos5@gmail.com>
2024-06-20 13:46:10 +0100

    Final Version

--- currencyRate/currencyRateApp/views.py
+++ currencyRate/currencyRateApp/views.py
@@ -36,18 +36,21 @@ def get_usd_to_eur_rate(request):
 
             if not start_date or not end_date:
                 return JsonResponse({'error': 'Invalid format. Use YYYY-MM-DD.'}, status=400)
-                
-            date_range = [start_date + timedelta(days=1) for i in range((end_date - start_date).days + 1)]
-            rates = []
 
+            if start_date == end_date:
+                try:
+                    rate = fetch_and_store_historical_rate(start_date)
+                    return JsonResponse({'USD_EUR': rate})
+                except Exception as e:
+                    return JsonResponse({'error': str(e)}, status=500)                        
+            
+            date_range = [start_date + timedelta(days=i) for i in range((end_date - start_date).days)]
+            rates = []
+            
             for single_date in date_range:
-                exchange_rate, created = ExchangeRate.objects.get_or_create(date=single_date)
-                if not created:
-                    try:
-                        rate = fetch_and_store_historical_rate(single_date)
-                    except Exception as e:
-                        return JsonResponse({'error': str(e)}, status=500)
-                rates.append(exchange_rate.rate)
+                rate = fetch_and_store_historical_rate(single_date)
+
+            rates.append(rate)
                         
             if not rates:
                 return JsonResponse({'error': 'No exchange rate data found for the given date range.'}, status=404)
